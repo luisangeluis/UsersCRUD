62e50508f708b5d6445d565c20e658b5946259a0
luisangeluis <luisangeluis@gmail.com>
2022-06-20 13:53:37 -0500

    Styles to card body

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -23,10 +23,16 @@ const User = ({
     <div className="col-md-6 col-lg-4 my-2 my-md-3">
       <div className="card p-2 p-md-3 rounded-2">
         <img src="" alt="" />
-        <div className="card-body">
-          <div className="title">{`${user.first_name} ${user.last_name}`}</div>
-          <div className="text">CORREO {user.email}</div>
-          <div className="text">CUMPLEAÑOS {user.birthday}</div>
+        <div className="card-body d-flex flex-column align-items-start justify-content-start">
+          <div className="title">
+            Nombre:<b> {`${user.first_name} ${user.last_name}`}</b>
+          </div>
+          <div className="text">
+            CORREO <b>{user.email}</b>
+          </div>
+          <div className="text">
+            CUMPLEAÑOS <b>{user.birthday}</b>
+          </div>
         </div>
         <div className="card-fotter text-end">
           <button
