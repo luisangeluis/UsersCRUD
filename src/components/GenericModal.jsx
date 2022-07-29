ee73c29273b1ea6fc307cc6f1aa16eb53c492811
luisangeluis <luisangeluis@gmail.com>
2022-07-29 17:58:01 -0500

    Maked generic modal

--- src/components/GenericModal.jsx
+++ src/components/GenericModal.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const GenericModal = ({ modalMessage, setModalMessage }) => {
   return (
-    <div className="container generic-modal_container">
-      <section className="generic-modal">
+    <div className="generic-modal_container">
+      <section className="generic-modal rounded-3">
         <p>{modalMessage.message}</p>
         <article className="generic-modal_body">
-          <button className="btn btn-primary" onClick={modalMessage.action}>
+          <button
+            className="btn btn-primary mx-1 mx-md-2"
+            onClick={modalMessage.action}
+          >
             Yes
           </button>
           <button
-            className="btn btn-secondary"
+            className="btn btn-secondary mx-1 mx-md-2"
             onClick={() => setModalMessage(undefined)}
           >
             No
